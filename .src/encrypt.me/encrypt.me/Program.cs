661fe7d80a0e4d5a69fb06b69086c898c83f4d1c
Ahri Fox <darkomega950@gmail.com>
2016-10-14 10:28:55 -0400

    Created the project files.

---
+++ .src/encrypt.me/encrypt.me/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// This is the main namespace.
+/// </summary>
+namespace encrypt.me
+{
+    /// <summary>
+    /// This is the main program class.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// This method is used for initilazation.
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
+        static void Main(string[] args)
+        {
+            // Call the main logic method with the command line arguments.
+        }
+    }
+
+    /// <summary>
+    /// This is the *main* class for the program. It will hold all of the logic functions.
+    /// </summary>
+    class Logic
+    {
+        public void Encrypt(string raw)
+        {
+
+        }
+
+        /// <summary>
+        /// This method will convert raw data, into an incrypted string.
+        /// </summary>
+        /// <param name="raw"></param>
+        /// <returns></returns>
+        public string RawToEnc(string raw)
+        {
+            return "";
+        }
+    }
+}
